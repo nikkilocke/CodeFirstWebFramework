26dd1840df7c770931ee313a8a948ecd8eab4240
Nikki Locke <nikki@trumphurst.com>
2022-11-10 11:48:03 +0000

    v1.3.82 Allowed overriding names in AccessLevel

--- User.cs
+++ User.cs
@@ -260,16 +260,16 @@ namespace CodeFirstWebFramework {
 		/// NB Must always have 0, "None" as the first item.
 		/// </summary>
 		public virtual IEnumerable<JObject> Select(int maxLevel = int.MaxValue) {
-			List<JObject> values = new List<JObject>();
-			values.Add(new JObject().AddRange("id", 0, "value", "None"));
+			Dictionary<int, JObject> values = new Dictionary<int, JObject>();
+			values[0] = new JObject().AddRange("id", 0, "value", "None");
 			foreach (FieldInfo c in GetType()
 					.GetFieldsInOrder(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
 					.Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(int))) {
 				int id = (int)c.GetRawConstantValue();
-				if(id > 0 && id <= maxLevel && !values.Any(v => v.AsInt("id") == id))
-					values.Add(new JObject().AddRange("id", id, "value", c.Name.UnCamel()));
+				if(id > 0 && id <= maxLevel)
+					values[id] = new JObject().AddRange("id", id, "value", c.Name.UnCamel());
 			}
-			return values.OrderBy(v => v.AsInt("id"));
+			return values.Values.OrderBy(v => v.AsInt("id"));
 		}
 	}
 
